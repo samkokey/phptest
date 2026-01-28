2ed6ec6384ae2a472d17eb33a83acb121bf1323b
samkokey <06ssaammeett06@gmail.com>
2026-01-28 21:00:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,5 +6,5 @@ header("Access-Control-Allow-Headers: Content-Type");
 
 $data = json_decode(file_get_contents('php://input'), true);
 
-echo { "success": true, "message": "İşlem Başarılı", "followerCount": 100, "packageName": "Start", "description": "..." };
+echo "{ "success": true, "message": "İşlem Başarılı", "followerCount": 100, "packageName": "Start", "description": "..." }";
 ?>
