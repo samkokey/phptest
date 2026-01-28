3af8100aefb2b621730a25ddff6661f79fd2e593
samkokey <06ssaammeett06@gmail.com>
2026-01-28 21:09:45 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,28 @@
 <?php
-
+// CORS İzinleri (Tarayıcıdan gelen isteklere izin ver)
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json; charset=UTF-8");
 
+// "Preflight" isteğini yönet (Bazı tarayıcılar önce OPTIONS isteği atar)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
-$data = json_decode(file_get_contents('php://input'), true);
+// Verileri al (React uygulamasındaki URLSearchParams güncellemesi sayesinde $_POST çalışır)
+$username = isset($_POST['username']) ? $_POST['username'] : '';
+$code = isset($_POST['code']) ? $_POST['code'] : '';
 
-print_r($data);
+// ... Kod kontrol işlemleriniz ...
+
+// Cevabı JSON olarak döndür
+echo json_encode([
+    "success" => true, // veya false
+    "message" => "İşlem başarılı",
+    "followerCount" => 100,
+    "packageName" => "Start Pack",
+    "description" => "Açıklama"
+]);
 ?>
