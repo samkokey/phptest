7d971e1bba32f6a9e40d387e413acb1691a309a7
samkokey <06ssaammeett06@gmail.com>
2026-01-28 20:40:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
+// Gelen JSON verisini oku
+$json_data = file_get_contents('php://input');
+$data = json_decode($json_data, true);
 
-print_r($_POST);
+// Veriyi işle
+$kullanici_adi = $data['code'] ?? 'Bilinmiyor';
+
+// Bir yanıt hazırla
+$response = [
+    "message" => "Merhaba " . $kullanici_adi . ", verin başarıyla alındı!"
+];
+
+// Yanıtı JSON formatında gönder
+header('Content-Type: application/json');
+echo json_encode($response);
+?>
