44fc1c4a7fb2fa5f5f22ed1ea0b55315f8fd792c
samkokey <06ssaammeett06@gmail.com>
2026-01-28 21:33:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,19 +10,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
 }
-
+include("kodlar.php");
 // Verileri al (React uygulamasındaki URLSearchParams güncellemesi sayesinde $_POST çalışır)
 $username = isset($_POST['username']) ? $_POST['username'] : '';
 $code = isset($_POST['code']) ? $_POST['code'] : '';
 
 // ... Kod kontrol işlemleriniz ...
 
-// Cevabı JSON olarak döndür
-echo json_encode([
+if ($code && array_key_exists($code, $codeDatabase)) {
+    // Kod bulundu
+    $found_item  = $codeDatabase[$code]['ACIKLAMA'];
+    $found_price = $codeDatabase[$code]['ADET'];
+    echo json_encode([
     "success" => true, // veya false
     "message" => "İşlem başarılı baba",
-    "followerCount" => 100,
-    "packageName" => "Start Pack",
+    "followerCount" => $found_price,
+    "packageName" => $found_item,
     "description" => "Açıklama"
-]);
+    ]);
+} else {
+    // Kod bulunamadı
+    http_response_code(404);
+    echo json_encode([
+    "success" => false
+    ]);
+}
+
+
+
+// Cevabı JSON olarak döndür
+
 ?>
